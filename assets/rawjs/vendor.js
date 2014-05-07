a41a68b2bea119fae8c2d1e2722214ccd560aa8a
Christian Blaschke <mail@platdesign.de>
2014-05-07 19:56:49 +0200

    twitter, github, download, styles

--- assets/rawjs/vendor.js
+++ assets/rawjs/vendor.js
@@ -2,3 +2,7 @@
 //= include ../../vendor/jquery/dist/jquery.js
 //= include ../../vendor/angular/angular.js
 //= include ../../vendor/angular-ui-router/release/angular-ui-router.js
+
+
+// Twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
