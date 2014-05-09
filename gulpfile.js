404bf517a94d400152aabf251aa924f3d3431b09
Christian Blaschke <mail@platdesign.de>
2014-05-09 07:40:13 +0200

    Auto-commit after build-task

--- gulpfile.js
+++ gulpfile.js
@@ -136,7 +136,7 @@ gulp.task('patch', ['build'], function(cb){
 			gulp.src( './' )
 				.pipe( git.add() )
 				.pipe( git.commit('Patch to v' + version) )
-				.pipe( git.tag('v'+version, params.desc || 'Autotag without description') )
+				.pipe( git.tag('v'+version, params.desc || 'Autotag without description'), {args: "signed"})
 			;
 
 		});
