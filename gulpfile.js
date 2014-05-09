62fd171c0e1cc7266ad689a16bab58c57de7b19d
Christian Blaschke <mail@platdesign.de>
2014-05-09 06:44:21 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var 
-		gulp 		= require('gulp')
+		fs			= require('fs')
+	,	gulp 		= require('gulp')
 	,	include 	= require('gulp-include')
 	,	watch 		= require('gulp-watch')
 	,	jshint 		= require('gulp-jshint')
@@ -7,81 +8,164 @@ var
 	,	concat 		= require('gulp-concat')
 	,	uglify 		= require('gulp-uglify')
 	,	rename 		= require('gulp-rename')
-
+	,	bump 		= require('gulp-bump')
+	,	git			= require('gulp-git')
+	,	semver		= require('semver')
 ;
 
+var getPkg = function () {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+};
+
+
+var paths = {
+	src: 	'./src',
+	dist: 	'./dist',
+	dev:{
+		main: ['./src/devonly.js', './src/fpdf.js'],
+		bundle: [
+			'vendor/stdClass/stdClass.js', 
+			'vendor/jspdf/dist/jspdf.debug.js',
+			'./src/devonly.js', 
+			'./src/fpdf.js'
+		]
+	},
+	build:{
+		main: './src/fpdf.js',
+		bundle: [
+			'vendor/stdClass/stdClass.js', 
+			'vendor/jspdf/dist/jspdf.debug.js',
+			'./src/fpdf.js'
+		]
+	}
+};
 
 
 
-gulp.task('dev', function(){
+gulp.task('dev-main', function(cb){
 
-	gulp.src('./src/**/*.js')
-		.pipe( watch(function(){
+	gulp.src( paths.dev.main )
+		.pipe( concat('fpdf.js') )
+		.pipe( include() )
+		.pipe( gulp.dest( paths.dist ) )
+		.on('end', cb)
+		.on('error', function(err){
+			console.log(err);
+			cb();
+		});
 
-			gulp.src(['./src/devonly.js', './src/fpdf.js'])
-				.pipe( concat('fpdf.js') )
-				.pipe( include() )
-			.pipe( gulp.dest('./dist') );
+});
 
-			// Bundle it with stdClass.js and jsPDF.js
-			gulp.src([
-				'vendor/stdClass/stdClass.js', 
-				'vendor/jspdf/dist/jspdf.debug.js',
-				'./src/devonly.js', 
-				'./src/fpdf.js'
-			])
-			.pipe( include() )
-			.pipe( concat('fpdf.bundled.js') )
-			
-			.pipe( gulp.dest('./dist') );
+gulp.task('dev-bundle', function(cb){
 
+	gulp.src( paths.dev.bundle )
+		.pipe( include() )
+		.pipe( concat('fpdf.bundled.js') )
+		.pipe( gulp.dest( paths.dist ) )
+		.on('end', cb)
+		.on('error', function(err){
+			console.log(err);
+			cb();
+		});
 
+});
 
+gulp.task('jshint', function(cb){
 
-
-
-
-			
-
-		}) )
+	gulp.src( paths.src + '/**/*.js' )
 		.pipe( jshint() )
-		.pipe( jshint.reporter(stylish) );
-	
-
+		.pipe( jshint.reporter( stylish ) )
+		.on('end', cb)
+		.on('error', function(err){
+			console.log(err);
+			cb();
+		});
 
 });
 
 
-gulp.task('build', function(){
 
-	gulp.src('./src/fpdf.js')
+
+gulp.task('build-main', function(cb){
+
+	gulp.src( paths.build.main )
 		.pipe( include() )
-		.pipe( jshint() )
-		.pipe( jshint.reporter(stylish) )
-		.pipe( gulp.dest('./dist') )
-		.pipe( rename('fpdf.min.js') )
 		.pipe( uglify({
 			outSourceMap: true,
 			//preserveComments: 'all'
 		}) )
-	.pipe( gulp.dest('./dist') );
+		.pipe( gulp.dest( paths.dist ) )
 
-
-
-
-	// Bundle it with stdClass.js and jsPDF.js
-	return gulp.src([
-		'vendor/stdClass/stdClass.js', 
-		'vendor/jspdf/dist/jspdf.debug.js',
-		'src/fpdf.js'
-	])
-	.pipe( include() )
-	.pipe( concat('fpdf.bundled.min.js') )
-	.pipe( uglify({
-		outSourceMap: true,
-	}) )
-	.pipe( gulp.dest('./dist') );
-
+		.on('end', cb)
+		.on('error', function(err){
+			console.log(err);
+			cb();
+		});
 
 });
+
+gulp.task('build-bundle', function(cb){
+
+	gulp.src( paths.build.bundle )
+		.pipe( include() )
+		.pipe( concat('fpdf.bundled.min.js') )
+		.pipe( uglify({
+			outSourceMap: true,
+			//preserveComments: 'all'
+		}) )
+		.pipe( gulp.dest( paths.dist ) )
+
+		.on('end', cb)
+		.on('error', function(err){
+			console.log(err);
+			cb();
+		});
+
+});
+
+
+
+gulp.task('dev', function(){
+	gulp.watch( paths.src + '/**/*.js' , ['jshint', 'dev-main', 'dev-bundle'])
+});
+
+
+gulp.task('build', ['jshint', 'dev-main', 'dev-bundle', 'build-main', 'build-bundle'], function(){
+
+	gulp.src( './*' )
+		.pipe( git.add() )
+//		.pipe( git.commit('Auto-commit after build-task') );
+
+});
+
+
+
+
+
+
+gulp.task('patch', function(){
+
+	var params 	= gulp.env;
+	var pkg 	= getPkg();
+
+	var version = semver.inc(pkg.version, 'patch');
+
+	gulp.src(['./bower.json', './package.json'])
+		.pipe( bump({ version:version, type: 'path', indent: 4 }))
+	.pipe(gulp.dest('./'));
+
+	git.tag('v'+version, params.desc || 'No description');
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
